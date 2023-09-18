71a9a34b244aee072e6d65db81764558fdb6fc8f
Ankur Kumar <98178905+Ankur-Kumar-4@users.noreply.github.com>
2023-09-18 12:10:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+const pre_element = document.getElementById("status");
+
+function textChange() {
+const new_h1 = document.createElement("h1");
+
+new_h1.textContent = "Entered Metaverse";
+pre_element.parentNode.replaceChild(new_h1,pre_element)	
+	
+}
+
